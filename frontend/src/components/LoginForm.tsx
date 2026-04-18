264c393cd1a277a461ce116bdb48133edb8a93cc
BIBS <breckynburke@gmail.com>
2026-04-18 02:05:58 -0600

    Missed Logo Path Variable Fixed

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const logo_path = "/public/logo.png";
+const logo_path = "/logo.png";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
